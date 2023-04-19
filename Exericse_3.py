1f0bc758ef84c5b18e9b96c0b342c20830fe3992
Patrick <lin880005@gmail.com>
2023-04-19 10:42:38 +0800

    first commit

---
+++ Exericse_3.py
@@ -0,0 +1,12 @@
+
+num=eval(input("請輸入整數:"))
+
+if num %7 == 0 & num %2 ==0:
+    print(f"{num}是2及7的倍數")
+elif num %2 == 0:
+    print(f"{num}是2的倍數")
+elif num %7 == 0:
+    print(f"{num}是7的倍數")
+else:
+    print(f"{num}不是2及7的倍數")
+
